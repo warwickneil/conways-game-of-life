ce1e140fe4697aad1a25fe2f24f30186fc14a379
Neil Warwick <NWarwick@nhc.ac.uk>
2016-03-14 15:42:21 +0000

    init commit

---
+++ Conways/Conways.GameOfLife/LifeRules.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Conways.GameOfLife
+{
+    public enum CellState
+    {
+        Alive,
+        Dead
+    }
+
+    public class LifeRules
+    {
+        public static CellState GetNewState(CellState currentState, int liveNeighbours)
+        {
+            if(!Enum.IsDefined(typeof(CellState), currentState))
+            {
+                throw new ArgumentOutOfRangeException(nameof(currentState));
+            }
+
+            if(liveNeighbours > 8 || liveNeighbours < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(liveNeighbours));
+            }
+
+            switch (currentState)
+            {
+                case CellState.Alive:
+                    if (liveNeighbours < 2 || liveNeighbours > 3)
+                    {
+                        return CellState.Dead;
+                    }
+                    break;
+                case CellState.Dead:
+                    if (liveNeighbours == 3)
+                    {
+                        return CellState.Alive;
+                    }
+                    break;
+                //default:
+                //    throw new ArgumentOutOfRangeException(nameof(currentState), currentState, null);
+            }
+
+            return currentState;
+        }
+    }
+}
---
+++ Conways/Conways.Tests/LifeRulesTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Conways.Tests
+{
+    using Conways.GameOfLife;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Any live cell with fewer than two live neighbours dies.
+    /// Any live cell with two or three live neighbours lives.
+    /// Any live cell with more than three live neighbours dies.
+    /// Any live cell with exactly three live neighbours becomes a live cell.
+    /// </summary>
+    [TestFixture]
+    public class LifeRulesTests
+    {
+        [Test]
+        public void LiveCell_FewerThan2Lives_Dies([Values(0, 1)] int liveNeighbours)
+        {
+            var currentState = CellState.Alive;
+            var newState = LifeRules.GetNewState(currentState, liveNeighbours);
+
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void LiveCell_2Or3LiveNeighbours_Lives([Values(2, 3)] int liveNeighbours)
+        {
+            var currentState = CellState.Alive;
+            var newState = LifeRules.GetNewState(currentState, liveNeighbours);
+
+            Assert.AreEqual(CellState.Alive, newState);
+        }
+
+        [Test]
+        public void LiveCell_MoreThan3LiveNeighbours_Dies([Range(4, 8)] int liveNeighbours)
+        {
+            var currentState = CellState.Alive;
+            var newState = LifeRules.GetNewState(currentState, liveNeighbours);
+
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void DeadCell_Exactly3LiveNeighbours_Lives()
+        {
+            var liveNeighbours = 3;
+            var currentState = CellState.Dead;
+            var newState = LifeRules.GetNewState(currentState, liveNeighbours);
+
+            Assert.AreEqual(CellState.Alive, newState);
+        }
+
+        [Test]
+        public void DeadCell_Fewer3LiveNeighbours_StaysDead([Range(0, 2)] int liveNeighbours)
+        {
+            var currentState = CellState.Dead;
+            var newState = LifeRules.GetNewState(currentState, liveNeighbours);
+
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void DeadCell_MoreThan3LiveNeighbours_StaysDead([Range(4, 8)] int liveNeighbours)
+        {
+            var currentState = CellState.Dead;
+            var newState = LifeRules.GetNewState(currentState, liveNeighbours);
+
+            Assert.AreEqual(CellState.Dead, newState);
+        }
+
+        [Test]
+        public void CurrentState_When2_ThrowArgumentException()
+        {
+            var currentState = (CellState)2;
+            var liveNeighbours = 0;
+
+            Assert.Throws(Is.TypeOf<ArgumentOutOfRangeException>(), 
+                          () => LifeRules.GetNewState(currentState, liveNeighbours));
+        }
+
+        [Test]
+        public void LiveNeighbours_MoreThan8_ThrowArgumentException()
+        {
+            var currentState = CellState.Alive;
+            var liveNeighbours = 9;
+            var paramName = "liveNeighbours";
+
+            Assert.Throws(
+                Is.TypeOf<ArgumentOutOfRangeException>()
+                .And.Property("ParamName")
+                .EqualTo(paramName),
+                () => LifeRules.GetNewState(currentState, liveNeighbours));
+        }
+
+        [Test]
+        public void LiveNeighbours_LessThan0_ThrowArgumentException()
+        {
+            var currentState = CellState.Alive;
+            var liveNeighbours = -1;
+            var paramName = "liveNeighbours";
+
+            Assert.Throws(
+                Is.TypeOf<ArgumentOutOfRangeException>()
+                .And.Property("ParamName")
+                .EqualTo(paramName),
+                () => LifeRules.GetNewState(currentState, liveNeighbours));
+        }
+    }
+}
